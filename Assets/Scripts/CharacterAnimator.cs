58700bf50b9c869d76c094a5ed4206c85df376f6
Jonah <jonahcheyette@gmail.com>
2021-01-14 17:15:49 -0800

    just finished putting in the astronaut

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    const float locomotionAnimationSmoothTime = 0.1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
