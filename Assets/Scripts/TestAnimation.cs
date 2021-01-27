6bb9d996703b5cfe203a8a4d684cb5b41f4418e7
Jonah <jonahcheyette@gmail.com>
2021-01-26 20:58:28 -0800

    got the fuckin animation in, dear lord that was awful

---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour {
+    Animator animator;
+    public bool open;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        animator.SetBool("doorIsOpen", open);
+    }
+
+    private void OnValidate() {
+        if (Application.isPlaying) {
+            //animator.SetBool("doorIsOpen", open);
+        }
+    }
+}
