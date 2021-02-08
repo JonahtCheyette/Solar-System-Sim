e03f5e0217624bf87c74b66ecbe5800987501df1
Jonah <jonahcheyette@gmail.com>
2021-02-07 19:20:14 -0800

    Got A new system for gravity in, refactored it as well

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour {
+    public const float gravityStrength = 1;
+
+    public float mass;
+
+    private CelestialBody[] bodies;
+
+    public void Start() {
+        List<CelestialBody> temp = new List<CelestialBody>();
+        bodies = FindObjectsOfType<CelestialBody>();
+        for(int i = bodies.Length - 1; i >= 0; i--) {
+            if(bodies[i] != this) {
+                temp.Add(bodies[i]);
+            }
+        }
+        bodies = temp.ToArray();
+    }
+    /*
+    public void AttractPlayer(Transform body, Rigidbody rigidBody, float playerMass) {
+        Vector3 targetDirection = (transform.position - body.position).normalized;
+        Vector3 bodyDown = -body.up;
+        //rotate the body so that its down points torwards the planet
+        body.rotation = Quaternion.FromToRotation(bodyDown, targetDirection) * body.rotation;
+        rigidBody.AddForce(targetDirection * (gravityStrength * mass * playerMass)/(body.position - transform.position).sqrMagnitude);
+    }*/
+}
