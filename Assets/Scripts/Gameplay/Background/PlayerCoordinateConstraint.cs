e3bd9d932c3d388c48e0de23930284035ae0f36b
Jonah <jonahcheyette@gmail.com>
2021-09-14 18:49:19 -0700

    constraining the character's coordinates to avoid running into float imprecision errors

---
+++ Assets/Scripts/Gameplay/Background/PlayerCoordinateConstraint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCoordinateConstraint : MonoBehaviour {
+    public float constraintLength = 100000f;
+
+    // Update is called once per frame
+    void Update() {
+        if(transform.position.magnitude > constraintLength) {
+            Vector3 currentPlayerPosition = transform.position;
+            GameObject[] objects = FindObjectsOfType<GameObject>();
+            foreach(GameObject obj in objects) {
+                if (!obj.transform.parent) {
+                    obj.transform.position -= currentPlayerPosition;
+                }
+            }
+        }
+    }
+}
